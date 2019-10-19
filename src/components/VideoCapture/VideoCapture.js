62431728bd44f386dda18190253b52e0b7e8c0a2
Felipe Torres <felipe@aifund.ai>
2019-10-19 12:20:12 -0500

    Aligning video and styling guidelines

--- src/components/VideoCapture/VideoCapture.js
+++ src/components/VideoCapture/VideoCapture.js
@@ -6,24 +6,14 @@ const VideoCapture = () => {
     const videoElement = useRef()
     const cameraStream = useCamera()
 
-    const videoStyle = {
-        width: '300px',
-        height: '300px'
-    }
-
     if (cameraStream) {
         videoElement.current.srcObject = cameraStream
         videoElement.current.play()
     }
 
     return (
-        <div style={videoStyle}>
-            <svg width="300" height="300">
-                <clipPath id="clip">
-                    <circle cx="150" cy="150" r="150"/>
-                </clipPath>
-            </svg>
-            <video className="VideoCapture" ref={videoElement} style={{...videoStyle, filter:"blur(3px)"}}></video>
+        <div className="videoCaptureContainer">
+            <video className="VideoCapture" ref={videoElement}></video>
         </div>
     )
 
